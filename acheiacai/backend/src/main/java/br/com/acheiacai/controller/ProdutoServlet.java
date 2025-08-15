c0ed55b7b9cb62fd66fa7c4a38da6349631e2c56
Alan Gonçalves <alannogueira314@gmail.com>
2025-08-15 15:51:59 -0300

    feat: Adição do metodo Post e Create no DB

--- acheiacai/backend/src/main/java/br/com/acheiacai/controller/ProdutoServlet.java
+++ acheiacai/backend/src/main/java/br/com/acheiacai/controller/ProdutoServlet.java
@@ -2,6 +2,7 @@ package br.com.acheiacai.controller;
 
 import br.com.acheiacai.dao.ProdutoDAO;
 import br.com.acheiacai.model.Produto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,20 +10,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @WebServlet("/produtos")
 public class ProdutoServlet extends HttpServlet{
+
+    ProdutoDAO prodDAO = new ProdutoDAO();
+    ObjectMapper conversor = new ObjectMapper();
     
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
             try {
-                ProdutoDAO prodDAO = new ProdutoDAO();
-                List<Produto> produtos = prodDAO.listarTodos();
-                ObjectMapper conversor = new ObjectMapper();
-
+                ArrayList<Produto> produtos = prodDAO.listarTodos();
                 String jsonProdutos = conversor.writeValueAsString(produtos);
                 response.setContentType("application/json");
                 response.setCharacterEncoding("UTF-8");
@@ -41,4 +44,54 @@ public class ProdutoServlet extends HttpServlet{
             }
 
         }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            String jsonString = request.
+                    getReader().
+                    lines().
+                    collect(Collectors.joining(System.lineSeparator()));
+
+            if (jsonString == null || jsonString.trim().isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().write("Erro: Corpo da requisição está vazio.");
+            }
+
+            Produto novoProduto = conversor.readValue(jsonString, Produto.class);
+            prodDAO.criarProduto(novoProduto);
+
+            response.setStatus(HttpServletResponse.SC_CREATED);
+            response.getWriter().write("Produto criado com sucesso!");
+
+
+        } catch (JsonProcessingException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400
+            response.getWriter().write("Erro: O JSON enviado é inválido. Detalhes: " + e.getMessage());
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Erro: Falha ao salvar o produto no banco de dados.");
+            e.printStackTrace();
+
+        } catch (Exception e) {
+
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Erro inesperado no servidor.");
+            e.printStackTrace();
+        }
+
+    }
+
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+    }
+
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException { }
 }
--- acheiacai/backend/src/main/java/br/com/acheiacai/dao/ProdutoDAO.java
+++ acheiacai/backend/src/main/java/br/com/acheiacai/dao/ProdutoDAO.java
@@ -29,33 +29,50 @@ public class ProdutoDAO{
 
     
 
-public ArrayList listarTodos() {
+    public ArrayList<Produto> listarTodos() {
 
-    String sql = "SELECT * FROM produtos";
-    ArrayList<Produto> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+        ArrayList<Produto> produtos = new ArrayList<>();
 
-    try (Connection conexao = getConexao();
-        PreparedStatement stmt = conexao.prepareStatement(sql);
-        ResultSet resultado = stmt.executeQuery()) {
+        try (Connection conexao = getConexao();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery()) {
 
-            while(resultado.next()) {
-                Long id = resultado.getLong("id");
-                String nome = resultado.getString("nome");
-                String tipo = resultado.getString("tipo");
-                String variacao = resultado.getString("variacao");
-                String tamanho = resultado.getString("tamanho");
-                BigDecimal preco = resultado.getBigDecimal("preco");
-                
-                Produto produto = new Produto(id, nome, tipo, variacao, tamanho, preco);
-                produtos.add(produto);
-                }
+                while(resultado.next()) {
+                    Long id = resultado.getLong("id");
+                    String nome = resultado.getString("nome");
+                    String tipo = resultado.getString("tipo");
+                    String variacao = resultado.getString("variacao");
+                    String tamanho = resultado.getString("tamanho");
+                    BigDecimal preco = resultado.getBigDecimal("preco");
 
-            } catch (SQLException e) {
-                    System.err.println("Erro ao listar produtos!");
-                    e.printStackTrace();
-                }
+                    Produto produto = new Produto(id, nome, tipo, variacao, tamanho, preco);
+                    produtos.add(produto);
+                    }
 
-        return produtos;
-    
+                } catch (SQLException e) {
+                        System.err.println("Erro ao listar produtos!");
+                        e.printStackTrace();
+                    }
+
+            return produtos;
+
+        }
+
+    public void criarProduto(Produto produto) throws SQLException {
+
+        String sql = "INSERT INTO produtos (nome, tipo, variacao, tamanho, preco)  VALUES(?, ?, ?, ?, ?)";
+
+        try (Connection conexao = getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+            stmt.setString(1, produto.nome());
+            stmt.setString(2, produto.tipo());
+            stmt.setString(3, produto.variacao());
+            stmt.setString(4, produto.tamanho());
+            stmt.setBigDecimal(5, produto.preco());
+            stmt.execute();
+
+        }
     }
 }
